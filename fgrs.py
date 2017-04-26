1db82df2f4c84a2b7d23d6ed347a1d7b98594781
ANIL KUMAR <anil946233@gmail.com>
2017-04-26 08:57:39 +0530

    Add fuzzy distance for all users

--- fgrs.py
+++ fgrs.py
@@ -12,16 +12,16 @@ def fuzzy_dist(a, b, A, B):
     return abs(a-b)*eucledian(A,B)
 
 def fuzzy_distance(ui, uj):
-    fuzzy_dis = []
+    fuzzy_dis = [0]*21
     for i in range(0,19):
-        ui_gim = np.array([g.very_bad(ui[i]), g.bad(ui[i]), g.average(ui[i]), g.good(ui[i]), g.very_good(ui[i]), g.excellent(ui[i])])
-        uj_gim = np.array([g.very_bad(uj[i]), g.bad(uj[i]), g.average(uj[i]), g.good(uj[i]), g.very_good(uj[i]), g.excellent(uj[i])])
-        fuzzy_dis.append(fuzzy_dist(ui[i], uj[i], ui_gim, uj_gim))
+        ui_gim = [g.very_bad(ui[i]), g.bad(ui[i]), g.average(ui[i]), g.good(ui[i]), g.very_good(ui[i]), g.excellent(ui[i])]
+        uj_gim = [g.very_bad(uj[i]), g.bad(uj[i]), g.average(uj[i]), g.good(uj[i]), g.very_good(uj[i]), g.excellent(uj[i])]
+        fuzzy_dis[i] = fuzzy_dist(ui[i], uj[i], ui_gim, uj_gim)
     
-    ui_gim = np.array([a.young(ui[i]), a.middle(ui[i]), a.old(ui[i])])
-    uj_gim = np.array([a.young(uj[i]), a.middle(uj[i]), a.old(uj[i])])
-    fuzzy_dis.append(fuzzy_dist(ui[i], uj[i], ui_gim, uj_gim))
-    print fuzzy_dis
+    ui_gim = [a.young(ui[i]), a.middle(ui[i]), a.old(ui[i])]
+    uj_gim = [a.young(uj[i]), a.middle(uj[i]), a.old(uj[i])]
+    fuzzy_dis[i] = fuzzy_dist(ui[i], uj[i], ui_gim, uj_gim)
+    return fuzzy_dis
 
 #print fuzzy_dist(35, 40, np.array([3, 4 ,5]), np.array([4, 5, 6]))
 #Data Details
@@ -52,20 +52,19 @@ for i in range(0,5):
     tau_data = df.loc[df['user_id'].isin(training_active_users)][:10]
     index = np.arange(0,tau_data.shape[0])
     i=0
-    print index
     for key, value in tau_data.iterrows():
         user_ui_movies = df.loc[df['user_id']==value['user_id']]
-        feature_array = []
+        
         feature_array = gim.gim_final(user_ui_movies, value['user_id'])
         #print 'GIM array', feature_array
         feature_array[19], feature_array[20] = value['age'], value['user_id']
         #print feature_array.shape
-        feature_row = pd.DataFrame(feature_array)
-        model_data_au = model_data_au.append(feature_row)
+        model_data_au.loc[i] = feature_array
+        i = i+1
     #print model_data_au
     #print model_data_au
     #Working with passive users
-    i=0
+    i=0    
     pu_data = df.loc[df['user_id'].isin(passive_users)][:10]
     
     for key, value in pu_data.iterrows():
@@ -74,8 +73,16 @@ for i in range(0,5):
         #print 'GIM array', feature_array
         feature_array_p[19], feature_array_p[20] = value['age'], value['user_id']
         #print feature_array.shape
-        feature_row = pd.DataFrame(feature_array_p)
-        model_data_pu = model_data_pu.append(feature_row)
-    print feature_array, feature_array_p
-    fuzzy_distance(feature_array, feature_array_p)
+        model_data_pu.loc[i] = feature_array_p
+        i = i+1
+    #print model_data_au
     #print model_data_pu
+    
+    fuzzy_df = pd.DataFrame(columns=m_cols)
+    for key, value in model_data_au.iterrows():
+        i=0
+        for key1, value1 in model_data_pu.iterrows():
+            fuzzy_df.loc[i] = fuzzy_distance(value, values1)
+            #print value[i], value1[i]
+            i = i+1
+    print 'Fuzzy Distanse between users: ',fuzzy_df
