1f9f567e36033031e46ba4c740c803af7a01e209
sunita@devi <anil946233@gmail.com>
2018-11-09 02:29:11 +0530

    Change comments and code

--- load_data.py
+++ load_data.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-#Load users
+# Load users
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
 users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols, encoding='latin-1')
-#load ratings
+
+# Load ratings
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols, encoding='latin-1')
-#load genres
+
+# Load genres
 i_cols = ['movie_id', 'movie_title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
  'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
  'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
 items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
  encoding='latin-1')
-#All in one DataFrame
+
+# All in one DataFrame
 mr_ur = pd.merge(users, ratings, on='user_id')
 df = pd.merge(mr_ur, items, on='movie_id')
