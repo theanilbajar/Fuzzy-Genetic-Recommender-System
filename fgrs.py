38b943cb15844d13856af089e5bd1fa19506ed51
Anil Kumar <anil946233@gmail.com>
2018-11-11 02:59:36 +0530

    Change logic, structure, and comments

--- fgrs.py
+++ fgrs.py
@@ -9,10 +9,28 @@ from settings import NO_OF_GENRES, NO_OF_FEATURES
 import load_data
 import fuzzy_sets
 import gim
+from genetic import genetic_optimize, cost_function
+
+# Constants
+WEIGHTS=np.random.rand(NO_OF_FEATURES)
+NO_OF_ITERATIONS=10
+NO_OF_NEIGHBOURS=20
+
+# namespace items_merged from load_data file
+items_merged = load_data.items_merged
 
 # Create objects for Age and GIM to use for fuzzy sets
 age = fuzzy_sets.Age()
 gim_obj = fuzzy_sets.GIM()
+
+m_cols = ['unknown', 'Action', 'Adventure',
+          'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+          'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western', 'age',
+          'user_id']
+
+# Initialize empty dataFrames for active and passive users
+model_data_active_users = pd.DataFrame(columns=m_cols)
+model_data_passive_users = pd.DataFrame(columns=m_cols)
 
 
 def euclidean_dist(list_a, list_b):
@@ -40,7 +58,32 @@ def fuzzy_distance(ui, uj):
     ui_gim = age.get_fuzzy_set(ui[i])
     uj_gim = age.get_fuzzy_set(uj[i])
     fuzzy_dis[i] = fuzzy_dist(ui[i], uj[i], ui_gim, uj_gim)
+
+    # adding user_id of second user
+    fuzzy_dis[NO_OF_FEATURES-1] = uj['user_id']
     return fuzzy_dis
+
+
+def get_neighbours(model_active_users, model_passive_users):
+
+    # Save active users and its neighbours in a data-frame with active users' id as column name
+    user_neighbours= pd.DataFrame(columns=model_active_users['user_id'])
+
+    # Iterate over active users model and save neighbours of each active users in user_neighbours
+    for _, value in model_active_users.iterrows():
+        j = 0
+        fuzzy_vec = []
+        for _, value_p in model_passive_users.iterrows():
+            fuzzy_vec.append(fuzzy_distance(value, value_p))
+
+            fuzzy_gen_dist = np.sum(np.multiply(WEIGHTS[:-1], np.array(fuzzy_vec[j][:-1]))) ** 0.5
+
+            fuzzy_vec[j] = [fuzzy_gen_dist, fuzzy_vec[j][-1]]
+
+            j = j + 1
+
+        user_neighbours[value[-1]] = [n[1] for n in sorted(fuzzy_vec, key=operator.itemgetter(0), reverse=True)][:NO_OF_NEIGHBOURS]
+    return user_neighbours
 
 
 def model_for_users(users_data):
@@ -53,14 +96,15 @@ def model_for_users(users_data):
     i = 0
     model_data_for_users = pd.DataFrame(columns=m_cols)
 
-    for key, value in users_data.iterrows():
+    for _, curr_value in users_data.iterrows():
+
         # Get user movies based on user
-        user_movies = items_merged.loc[items_merged['user_id'] == value['user_id']]
+        user_movies = items_merged.loc[items_merged['user_id'] == curr_value['user_id']]
 
         # Get feature list for all movies of one user
-        feature_array = gim.gim_final(user_movies, value['user_id'])
-        feature_array[NO_OF_GENRES] = value['age']
-        feature_array[NO_OF_GENRES + 1] = value['user_id']
+        feature_array = gim.gim_final(user_movies, curr_value['user_id'])
+        feature_array[NO_OF_GENRES] = curr_value['age']
+        feature_array[NO_OF_GENRES + 1] = curr_value['user_id']
 
         # Save current feature values in model data
         model_data_for_users.loc[i] = feature_array
@@ -68,75 +112,72 @@ def model_for_users(users_data):
     return model_data_for_users
 
 
-def recommend(nearest_neighbours, test_examples):
+def recommend(nearest_neighbours, test_users_data):
     """Recommend rating for given movies i.e. test_examples based on nearest neighbours.
 
-    Also return average absolute error for each fold.
+    Also return actual and predicated ratings for testing users
     """
-    ms = 0
-    for key, item in test_examples.iterrows():
-        actual_rating, m_id = item['rating'], item['movie_id']
+    predicated_rat=[]
+    actual_rat = []
+    for key, item in test_users_data.iterrows():
+        m_id = item['movie_id']
         n_ratings = []
         for i in nearest_neighbours:
+
+            # Get items or movie details reviewed by neighbour i with given m_id
             temp = items_merged.loc[items_merged['user_id'] == i].loc[items_merged['movie_id'] == m_id]
             for k, it in temp.iterrows():
                 n_ratings.append(it['rating'])
-        predicated_rating = float(sum(n_ratings)) / len(n_ratings) if len(n_ratings) else 0
-        ms += abs(predicated_rating-actual_rating)
-    return ms/test_examples.shape[0]
-
-m_cols = ['unknown', 'Action', 'Adventure',
-          'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
-          'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western', 'age',
-          'user_id']
-
-items_merged = load_data.items_merged
-
-model_data_active_users = pd.DataFrame(columns=m_cols)
-model_data_passive_users = pd.DataFrame(columns=m_cols)
+        predicated_rat.append(float(sum(n_ratings)) / len(n_ratings) if len(n_ratings) else 0)
+        actual_rat.append(item['rating'])
+    return actual_rat, predicated_rat
 
 # Users who has rated movies at least 60 movies
 top_users = load_data.items_merged.groupby('user_id').size().sort_values(ascending=False)[:497]
 
-for i in range(0, 5):
+model_error = []
 
-    # Get 10% of the top_users as active users and remaining are passive users
+# Train model for given iterations
+for i in range(0, NO_OF_ITERATIONS):
+
+    # Get random 10% of the top_users as active users and remaining are passive users
     active_users = top_users.sample(frac=0.10)
+
+    # Random 34% of active users will be used for training and 66% users for testing purpose.
+    training_active_users = active_users.sample(frac=0.34)
+    #testing_active_users = active_users.drop(training_active_users.index)
 
     # passive_users will be used as training examples
     passive_users = top_users.drop(active_users.index)
 
-    # 34% of active users will be used for training and 66% users for testing purpose.
-    training_active_users = active_users.sample(frac=0.34)
-    testing_active_users = active_users.drop(training_active_users.index)
-
     # Get active and passive users' data from merged movies, items, and users
-    active_users_data = items_merged.loc[items_merged['user_id'].isin(training_active_users)][:10]
+    training_active_users_data = items_merged.loc[items_merged['user_id'].isin(training_active_users)][:10]
+    test_active_users_data = items_merged.loc[items_merged['user_id'].isin(training_active_users)][10:]
     passive_users_data = items_merged.loc[items_merged['user_id'].isin(passive_users)][:10]
 
     # Get model for active users
-    model_data_active_users = model_for_users(active_users_data)
+    model_data_active_users = model_for_users(training_active_users_data)
 
     # Get model for passive users
     model_data_passive_users = model_for_users(passive_users_data)
 
-    fuzzy_vec = []
-    error = []
-    for key, value in model_data_active_users.iterrows():
-        i = 0
-        for key_p, value_p in model_data_passive_users.iterrows():
+    # Get neighbour users of active users
+    active_users_neighbours = get_neighbours(model_data_active_users, model_data_passive_users)
 
-            fuzzy_vec.append(fuzzy_distance(value, value_p))
+    # Recommend users based on neighbours
+    actual_ratings, predicated_ratings=recommend(active_users_neighbours, test_active_users_data)
 
-            fuzzy_vec[i] = [(sum(x for x in fuzzy_vec[i][:-1]))**0.5, fuzzy_vec[i][-1]]
-            i = i + 1
+    # Get error for predictions of test users
+    cur_error = cost_function(actual_ratings, predicated_ratings)
 
-        neighbours = [n[1] for n in sorted(fuzzy_vec, key=operator.itemgetter(0), reverse=True)][:30]
+    # Optimize weights using genetic algorithm approach and update weighs
+    WEIGHTS = genetic_optimize(actual_ratings, predicated_ratings).flatten()
 
-        testing_user = items_merged.loc[items_merged['user_id'] == value['user_id']].sample(frac=0.66)
+    # Add current iteration error to model error for MAE of the model
+    model_error.append(cost_function(actual_ratings, predicated_ratings))
 
-        # Recommend users based on neighbours
-        error.append(recommend(neighbours, testing_user))
-
-    MAE = (sum(error)/len(error))*100
-    print("Mean Absolute Error is: ", MAE)
+    # Log details for current iteration
+    print("Iteration : ", i)
+    print("Error: ", cur_error)
+    print('Weights after iteration is: ', WEIGHTS)
+print("Mean absolute error for all iterations: ", sum(model_error)/len(model_error))
